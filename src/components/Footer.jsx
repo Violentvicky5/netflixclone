8e82505f1037317ab041129617782c7b0adce526
unknown <violentvicky5@gmail.com>
2025-11-19 15:09:17 +0530

    correction in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,80 +3,170 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-    <div className="container">
-      <div className="row mt-5">
-        <div className="col-12 para text-white para">Questions? Call <a href="#"> 000-800-919-1743</a> </div>
-        {/* First Column */}
-        <div className="col-4 my-3">
-           
-          <ul className="nav flex-column">
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0  text-white para" style={{textDecoration:"underline"}}>Faq</a>
-            </li>
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0  text-white para" style={{textDecoration:"underline"}}>Media Centre</a>
-            </li>
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0  text-white para" style={{textDecoration:"underline"}}>Ways to watch</a>
-            </li>
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0  text-white para" style={{textDecoration:"underline"}}>Cookie Preferences</a>
-            </li>
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0  text-white para" style={{textDecoration:"underline"}}>Speed Test</a>
-            </li>
-          </ul>
-        </div>
+      <div className="container">
+        <div className="row mt-5">
+          <div className="col-12 para text-white para">
+            Questions? Call <a href="#"> 000-800-919-1743</a>{" "}
+          </div>
+          {/* First Column */}
+          <div className="col-4 my-3">
+            <ul className="nav flex-column">
+              <li className="nav-item mb-2">
+                <a
+                  href="#"
+                  className="nav-link p-0  text-white para"
+                  style={{ textDecoration: "underline" }}
+                >
+                  Faq
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a
+                  href="#"
+                  className="nav-link p-0  text-white para"
+                  style={{ textDecoration: "underline" }}
+                >
+                  Media Centre
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a
+                  href="#"
+                  className="nav-link p-0  text-white para"
+                  style={{ textDecoration: "underline" }}
+                >
+                  Ways to watch
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a
+                  href="#"
+                  className="nav-link p-0  text-white para"
+                  style={{ textDecoration: "underline" }}
+                >
+                  Cookie Preferences
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a
+                  href="#"
+                  className="nav-link p-0  text-white para"
+                  style={{ textDecoration: "underline" }}
+                >
+                  Speed Test
+                </a>
+              </li>
+            </ul>
+          </div>
 
-        {/* Second Column */}
-        <div className="col-4 my-3">
-           
-          <ul className="nav flex-column">
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0  text-white para" style={{textDecoration:"underline"}}>Help Centre</a>
-            </li>
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0  text-white para" style={{textDecoration:"underline"}}>Investor Relations</a>
-            </li>
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0  text-white para" style={{textDecoration:"underline"}}>Terms OF Use</a>
-            </li>
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0  text-white para" style={{textDecoration:"underline"}}>Corporate Information</a>
-            </li>
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0  text-white para" style={{textDecoration:"underline"}}>Legal Notices</a>
-            </li>
-          </ul>
-        </div>
+          {/* Second Column */}
+          <div className="col-4 my-3">
+            <ul className="nav flex-column">
+              <li className="nav-item mb-2">
+                <a
+                  href="#"
+                  className="nav-link p-0  text-white para"
+                  style={{ textDecoration: "underline" }}
+                >
+                  Help Centre
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a
+                  href="#"
+                  className="nav-link p-0  text-white para"
+                  style={{ textDecoration: "underline" }}
+                >
+                  Investor Relations
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a
+                  href="#"
+                  className="nav-link p-0  text-white para"
+                  style={{ textDecoration: "underline" }}
+                >
+                  Terms OF Use
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a
+                  href="#"
+                  className="nav-link p-0  text-white para"
+                  style={{ textDecoration: "underline" }}
+                >
+                  Corporate Information
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a
+                  href="#"
+                  className="nav-link p-0  text-white para"
+                  style={{ textDecoration: "underline" }}
+                >
+                  Legal Notices
+                </a>
+              </li>
+            </ul>
+          </div>
 
-        {/* Third Column */}
-        <div className="col-4 my-3 ">
-           
-          <ul className="nav flex-column">
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0  text-white para" style={{textDecoration:"underline"}} style={{textDecoration:"underline"}}>Account</a>
-            </li>
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0  text-white para" style={{textDecoration:"underline"}}>Jobs</a>
-            </li>
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0  text-white para" style={{textDecoration:"underline"}}>Privacy</a>
-            </li>
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0  text-white para" style={{textDecoration:"underline"}}>Contact Us</a>
-            </li>
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0  text-white para" style={{textDecoration:"underline"}}>Only on Netflix</a>
-            </li>
-          </ul>
-        </div>
+          {/* Third Column */}
+          <div className="col-4 my-3 ">
+            <ul className="nav flex-column">
+              <li className="nav-item mb-2">
+                <a
+                  href="#"
+                  className="nav-link p-0  text-white para"
+                  style={{ textDecoration: "underline" }}
+                >
+                  Account
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a
+                  href="#"
+                  className="nav-link p-0  text-white para"
+                  style={{ textDecoration: "underline" }}
+                >
+                  Jobs
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a
+                  href="#"
+                  className="nav-link p-0  text-white para"
+                  style={{ textDecoration: "underline" }}
+                >
+                  Privacy
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a
+                  href="#"
+                  className="nav-link p-0  text-white para"
+                  style={{ textDecoration: "underline" }}
+                >
+                  Contact Us
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a
+                  href="#"
+                  className="nav-link p-0  text-white para"
+                  style={{ textDecoration: "underline" }}
+                >
+                  Only on Netflix
+                </a>
+              </li>
+            </ul>
+          </div>
 
-       <p className="para text-white para">Netflix India</p>
-       <p className="para text-white para">This page is protected by Google reCAPTCHA to ensure you're not a bot. <a href="#">Learn more.</a> </p>
-        
-      </div>
-     
+          <p className="para text-white para">Netflix India</p>
+          <p className="para text-white para">
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. <a href="#">Learn more.</a>{" "}
+          </p>
+        </div>
       </div>
     </>
   );
