2f1ab922253941ad82e8151d5fff72ae3388b580
unknown <violentvicky5@gmail.com>
2025-11-19 15:13:31 +0530

    correction in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import logoImg from "../assets/logo.png"
 const Header = () => {
   return (
     <>
     <nav className=''>
         <div className='container navAlign'>
-        <a href="#"><img src="src/assets/logo.png" alt="logo" style={{height:"60px"}} /></a>
+        <a href="#"><img src={logoImg} alt="logo" style={{height:"60px"}} /></a>
         <a href="#" className='loginBtn btn text-white '>Sign In</a>
     </div>
     </nav>
